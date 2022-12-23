1fec16e815ead94a23322a3b15a8a30dcea55aec
Roeen <roeenacal@gmail.com>
2022-12-22 21:28:10 -0800

    Add alternative JS

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const accordion = document.getElementsByClassName('content-container');
+const accordion = document.querySelectorAll('.content-container');
 
 for ( i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
         this.classList.toggle('active');
     })
 };
+
+// We can also use the forEach method
+
+// accordion.forEach(e => {
+//     e.addEventListener('click', () => {
+//         e.classList.toggle('active');
+//     })
+// })
+
+// or
+
+// accordion.forEach(e => {
+//     e.addEventListener('click', function () {
+//         this.classList.toggle('active');
+//     })
+// })
